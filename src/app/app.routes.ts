570274e5c3ebbd6ddf1a0c9c220e9549041b8c39
MrCosik <cirocki.jakub1@gmail.com>
2025-03-29 10:51:33 +0100

    Clean up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
-import {Routes} from '@angular/router';
-import {HomePageComponent} from './pages/home-page/home-page.component';
+import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent
+    component: HomePageComponent,
   },
   {
     path: '**',
-    component: HomePageComponent
-  }
-
+    component: HomePageComponent,
+  },
 ];
