38d0ca9d8e6a606ba02f1fc79d4e404bd9a38d80
MrCosik <cirocki.jakub1@gmail.com>
2025-03-29 13:15:29 +0100

    Added some styling and background image

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [MatButton],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
