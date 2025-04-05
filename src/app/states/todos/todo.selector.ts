2bd7eb1fdb1b61eb2c473c447ba4364861f98fa7
Jakub Cirocki <56444584+MrCosik@users.noreply.github.com>
2025-04-05 18:59:28 +0200

    Removed unused function

--- src/app/states/todos/todo.selector.ts
+++ src/app/states/todos/todo.selector.ts
@@ -1,10 +1,4 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { createFeatureSelector } from '@ngrx/store';
 import { Todo } from '../../pages/todo-page/todo-page.component';
 
 export const selectTodos = createFeatureSelector<Todo[]>('todos');
-
-export const selectCollection = createFeatureSelector<number[]>('collection');
-
-export const selectTodoCollection = createSelector(selectTodos, selectCollection, (todos, collection) => {
-  return collection.map(id => todos.find(todo => todo.id === id)!);
-});
